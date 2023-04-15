b494fa74238bcf5cdc0ce941a5f4852d3856c188
Peter Simone <plssimone@gmail.com>
2023-04-15 17:23:05 +0900

    update to new contract

--- pages/api/balance/[slug].ts
+++ pages/api/balance/[slug].ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ethers } from "ethers";
 
-import abi from "../../abi";
+import abi from "../../../truffle/abis/smartSAFTAgreement.json";
 
 const docMapper = {
   SAFT: process.env.CONTRACT_ADDRESS_SAFT,
@@ -13,13 +13,21 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
   const { slug, address } = req.query;
   const docType = slug?.toString().toUpperCase() || "SAFT";
   const contractAddress = docMapper[docType as keyof typeof docMapper] as string;
+  console.log(contractAddress);
+  console.log(address);
 
   const provider = new ethers.JsonRpcProvider("https://rpc-mumbai.maticvigil.com");
 
   const contract = new ethers.Contract(contractAddress, abi, provider);
-  contract.balanceOf(address).then((result) => {
-    return res.status(200).json({ result });
-  });
+  contract
+    .ownedTokenIds(address, 1)
+    .then((result) => {
+      return res.status(200).json({ result });
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(404).send("BAD");
+    });
 };
 
 export default handler;
